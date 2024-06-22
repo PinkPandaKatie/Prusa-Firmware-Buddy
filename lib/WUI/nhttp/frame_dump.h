a39114a7af11206759f5ffba914f7a2aa2f42431
Katie Rust <katie@ktpanda.org>
2024-06-22 16:45:59 -0500

    Support QOI encoding when dumping frame buffer

--- lib/WUI/nhttp/frame_dump.h
+++ lib/WUI/nhttp/frame_dump.h
@@ -12,15 +12,33 @@
 
 namespace nhttp::printer {
 
+typedef union {
+	struct { unsigned char r, g, b, a; } rgba;
+	unsigned int v;
+} qoi_rgba_t;
+
 class FrameDump {
 private:
+    enum FrameDumpState {
+        HTTPHeaders,
+        QOIHeader,
+        QOIData,
+        QOIEnd,
+        Done
+    };
+
+    FrameDumpState state = FrameDumpState::HTTPHeaders;
+
     point_ui16_t pos;
-    bool headers_sent = false;
-    bool done = false;
+    int qoi_run;
+	qoi_rgba_t px_prev;
+    uint8_t qoi_index[64 * 3];
+
+    int debug_flags;
     bool can_keep_alive;
 
 public:
-    FrameDump(bool can_keep_alive);
+    FrameDump(bool can_keep_alive, int debug_flags=0);
     bool want_read() const { return false; }
     bool want_write() const { return true; }
     int read_some_pixels(uint8_t *buffer, size_t buffer_size);
